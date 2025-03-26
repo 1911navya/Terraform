e5569e6cc068934e9738fd61b05c0f10f8ca9095
1911navya <navya.jaya19@gmail.com>
2025-03-26 10:59:56 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "three" {
+  ami           = "ami-04aa00acb1165b32a"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "abc-server"
+  }
+  provisioner "local-exec" {
+    command = "echo this is my local server"
+  }
+}
